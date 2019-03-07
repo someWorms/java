d25736133df704a334439c51a783bee8d68bccc8
Ivan <prsw@bk.ru>
2019-03-07 14:56:55 +0100

    Added loop to main menu, deleted speed class

--- Exercices/src/Converter/MainClass.java
+++ Exercices/src/Converter/MainClass.java
@@ -10,36 +10,42 @@ import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Выберете велечину: \n1 - Вес\n2 - Длинна\n3 - Скорость");
-        Scanner sc = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in);
-        Scanner sc3 = new Scanner(System.in);
-        int choice = sc.nextInt();
-        if (choice == 1) {
-            Calcs c = new Massa();
-            System.out.println("Введите число");
-            c.setEntryNum(sc3.nextInt());
+        boolean checked = false;
 
-            System.out.println("В какой еденице заданное число? Укажите название");
-            System.out.println("1)Грамм (g)\n2)Килограмм (kg)");
-            System.out.println("3)Тонна (t)\n4)Килотонна (kt)");
+        while (!checked) {
+            System.out.println("Выберете велечину: \n1 - Вес\n2 - Длинна\n3 - Скорость");
+            Scanner sc = new Scanner(System.in);
+            Scanner sc2 = new Scanner(System.in);
+            Scanner sc3 = new Scanner(System.in);
+            int choice = sc.nextInt();
+            if (choice == 1) {
 
-            c.setTypo(sc2.nextLine());
-            c.doCalcs();
+                Calcs c = new Massa();
+                System.out.println("Введите число");
+                c.setEntryNum(sc3.nextInt());
+
+                System.out.println("В какой еденице заданное число? Укажите название");
+                System.out.println("1)Грамм (g)\n2)Килограмм (kg)");
+                System.out.println("3)Тонна (t)\n4)Килотонна (kt)");
+                checked = true;
+                c.setTypo(sc2.nextLine());
+                c.doCalcs();
+            }
+            if (choice == 2) {
+                Calcs c = new Lengths();
+                System.out.println("Введите число");
+                c.setEntryNum(sc3.nextInt());
+
+                System.out.println("В какой еденице заданное число? Укажите название");
+                System.out.println("1)Миллиметр (mm)\n2)Сантиметр (cm)");
+                System.out.println("3)Метр (m)\n4)Километр (km)");
+                checked = true;
+                c.setTypo(sc2.nextLine());
+                c.doCalcs();
+            }
+            if (choice < 1 || choice > 2) {
+                System.out.println("тут нету ниче");
+            }
         }
-        if (choice == 2){
-            Calcs c = new Lengths();
-            System.out.println("Введите число");
-            c.setEntryNum(sc3.nextInt());
-
-            System.out.println("В какой еденице заданное число? Укажите название");
-            System.out.println("1)Миллиметр (mm)\n2)Сантиметр (cm)");
-            System.out.println("3)Метр (m)\n4)Километр (km)");
-
-            c.setTypo(sc2.nextLine());
-            c.doCalcs();
-        }
-
     }
-
 }
