a77684360b826f930af0de6be687a95b4c7aba25
Ivan <prsw@bk.ru>
2019-03-10 09:53:07 +0100

    ex66

--- Exercises/src/ru/mccme/exercise65/CuantOfNumbs.java
+++ Exercises/src/ru/mccme/exercise65/CuantOfNumbs.java
@@ -25,5 +25,6 @@ public class CuantOfNumbs {
 
         System.out.println("Количесвто положительных чисел " + cuant);
 
+
     }
 }
---
+++ Exercises/src/ru/mccme/exercise66/CuantityBigger.java
@@ -0,0 +1,28 @@
+package ru.mccme.exercise66;
+
+import java.util.Scanner;
+
+//Дан массив, состоящий из целых чисел. Напишите программу,
+//которая подсчитает количество элементов массива, больших предыдущего (элемента с предыдущим номером).
+
+
+public class CuantityBigger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int[] arr = new int[x];
+        int elem = 0;
+
+        for (int i = 0; i < x; i++)
+            arr[i] = sc.nextInt();
+
+        for (int i = 1; i < x; i++){
+            if(arr[i] > arr[i-1]){
+                elem++;
+            }
+            else
+                elem = 0;
+        }
+        System.out.println(elem);
+    }
+}
