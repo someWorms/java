69952ae0e6f737ed82fddc69e23e165c5bd95816
Ivan <prsw@bk.ru>
2019-03-25 01:29:53 +0100

    cards

--- Exercises/src/DurakGame/Shuffle.java
+++ Exercises/src/DurakGame/Shuffle.java
@@ -9,12 +9,26 @@ public class Shuffle {
     int deckSize;
 
     void doShuffle(StandardCardDeck[] cards) {
-        StandardCardDeck[] shuffeled = new StandardCardDeck[cards.length];
-        for (int i = 0; i < cards.length; i++) {
-            shuffeled[i] = cards[i];
-            //algorithm to shuffle cards
-            System.out.println(shuffeled[i]);
-        }
+        StandardCardDeck[] shuffled = new StandardCardDeck[cards.length];
 
+        int[] collector = new int[8];
+        for (int i = 0; i < cards.length; i++) {
+
+            int x = (int)(Math.random()*8);
+
+            for (int j = 0; j < cards.length; j++) {
+                if(collector[j] == x){
+                    x = (int)(Math.random()*8);
+                    j = 0;
+                }else
+                    break;
+            }
+            shuffled[i] = cards[x];
+            collector[i]=x;
+
+
+            System.out.println(shuffled[i] + " номер коллектора " + collector[i]);
+
+        }
     }
 }
